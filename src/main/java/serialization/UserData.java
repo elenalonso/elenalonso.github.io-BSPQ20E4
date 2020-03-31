31137da445125ae199c422f0839290f9c5b7af66
eguiwow <ander.eguiluz@opendeusto.es>
2020-03-31 14:06:15 +0200

    Client/Server/Serialization #BSPQ20E4-26 add work 2h Development
    Classes have been created and sorted in packages, although they are not
    operational yet. They have been copied from "Simple example Jersey JDO",
    now I'm trying to adapt them and suit them in our project.

---
+++ src/main/java/serialization/UserData.java
@@ -0,0 +1,51 @@
+package serialization;
+
+public class UserData {
+
+    private String login;
+    private String password;
+    private String nickname;
+    private String email;
+
+
+	public UserData() {
+
+    }
+
+    public String getLogin() {
+        return this.login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String toString() {
+        return "[login=" + login + ", password=" + password + "]";
+    }
+    
+    public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
