e5be6cf3b6708f8b250a07e9016caf14f50d42d3
Borjados <borja.diazdeotazu@opendeusto.es>
2020-04-01 10:56:49 +0200

    #BSPQ20E4-22 add work 4h Development
    Forgot to add the "work" command in the previous commit

--- src/main/java/windows/main.java
+++ src/main/java/windows/main.java
@@ -2,9 +2,9 @@ package windows;
 
 public class main {
 	public static void main(String[] args) {
-		UserLogin us = new UserLogin();
-		us.setSize(500, 350);
-		us.setVisible(true);
+		UserLogin u = new UserLogin();
+		u.setSize(500, 350);
+		u.setVisible(true);
 	}
 }
 
