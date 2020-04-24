d8965edea664dba90a058860a8e0281178398fb4
eguiwow <ander.eguiluz@opendeusto.es>
2020-04-24 21:31:00 +0200

    Changes in FilmListUI

--- src/main/java/ui/FilmListUI.java
+++ src/main/java/ui/FilmListUI.java
@@ -30,9 +30,21 @@ public class FilmListUI extends JFrame{
 		this.filmList = flData;
 		this.setTitle(flData.getName());
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // cierra la ventana y se para la ejecución
-		setSize(740,480);
+		setSize(640,380);
 		setLocation(600,175);
-		setLayout(new BorderLayout());
+		setResizable(false);
+		
+		//Buttons
+		backbtn = new JButton("<<");
+		backbtn.setBounds(10, 10, 50, 25);
+		addFilm = new JButton("ADD new");
+		addFilm.setBounds(440, 280, 100, 25);
+		deleteFilm = new JButton("DELETE");
+		deleteFilm.setBounds(100, 280, 100, 25);
+		getContentPane().add(backbtn);
+		getContentPane().add(addFilm);
+		getContentPane().add(deleteFilm);
+		
 		
 		dlmFilms = new DefaultListModel<>();
 		liFilms = new JList<String>(dlmFilms);
@@ -44,11 +56,11 @@ public class FilmListUI extends JFrame{
 			logger.debug("No Films in this FilmList yet");
 		}
 
-		
+		liFilms.setBounds(220,200,100,100);
 		JPanel pCentral = new JPanel();
 		pCentral.add(liFilms);
 		
-		getContentPane().add(pCentral, "Center");
+		getContentPane().add(pCentral);
 		
 	}
 	
@@ -56,6 +68,11 @@ public class FilmListUI extends JFrame{
 		
 		FilmListData flData = new FilmListData();
 		ArrayList<FilmData> fl = new ArrayList<>();
+		for(int i=0;i<5;i++) {
+			FilmData a = new FilmData();
+			a.setTitle("Peli"+i);
+			fl.add(a);
+		}
 		flData.setFilmList(fl);
 		flData.setName("A1");
 		FilmListUI ui = new FilmListUI(flData);
