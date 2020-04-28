896a18d6ecb18a854519feef5961095aebbda59c
Marcoos01 <33113742+Marcoos01@users.noreply.github.com>
2020-04-28 19:41:58 +0200

    Added ContiPerf dependency to pom.xml Added one performance test.

--- src/test/java/testing/EasyFilmControllerTest.java
+++ src/test/java/testing/EasyFilmControllerTest.java
@@ -2,13 +2,25 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class EasyFilmControllerTest {
-
+	
+	private int num1 = 2;
+	private int num2 = 0;
+	@Rule
+	public ContiPerfRule i = new ContiPerfRule();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@PerfTest(invocations = 100)
+	@Required(throughput = 10)
+	public void test123() {
+		num2 = num2 + 2;
+		assertEquals(num1, num2);
 	}
 
 }
